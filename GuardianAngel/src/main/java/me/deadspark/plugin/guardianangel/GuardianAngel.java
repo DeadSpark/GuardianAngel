66d1dd8f44926c1bad8d38ec9a9251fd8816db03
DeadSpark <deadspark143@gmail.com>
2020-07-15 09:47:08 +0530

    Update GuardianAngel.java

--- GuardianAngel/src/main/java/me/deadspark/plugin/guardianangel/GuardianAngel.java
+++ GuardianAngel/src/main/java/me/deadspark/plugin/guardianangel/GuardianAngel.java
@@ -6,13 +6,9 @@ import me.deadspark.plugin.guardianangel.commands.God;
 import me.deadspark.plugin.guardianangel.commands.Heal;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
+import org.bukkit.Bukkit;
 
 public final class GuardianAngel extends JavaPlugin {
-
-    public static final String RESET = "\033[0m";
-    public static final String YELLOW_BOLD = "\033[1;33m";
-    public static final String CYAN = "\033[0;36m";
-    public static final String GREEN = "\033[0;32m";
 
     @Override
     public void onEnable() {
@@ -21,18 +17,14 @@ public final class GuardianAngel extends JavaPlugin {
 
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-
-        String pversion = getConfig().getString("Version");
-
-        System.out.println(YELLOW_BOLD+"["+CYAN+"GuardianAngel"+YELLOW_BOLD+"] "+GREEN+"Plugin made by DeadSpark"+RESET);
-        System.out.println(YELLOW_BOLD+"["+CYAN+"GuardianAngel"+YELLOW_BOLD+"] "+GREEN+"Version "+pversion+" has been Enabled"+RESET);
-
+        
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD.toString() + "Plugin made by DeadSpark");
         // Registering command
         
-        Objects.requireNonNull(getCommand("fly")).setExecutor(new Fly());
-        Objects.requireNonNull(getCommand("god")).setExecutor(new God());
-        Objects.requireNonNull(getCommand("heal")).setExecutor(new Heal());
-        Objects.requireNonNull(getCommand("feed")).setExecutor(new Feed());
+        getCommand("fly").setExecutor(new Fly());
+        getCommand("god").setExecutor(new God());
+        getCommand("heal").setExecutor(new Heal());
+        getCommand("feed").setExecutor(new Feed());
 
     }
 
@@ -41,8 +33,7 @@ public final class GuardianAngel extends JavaPlugin {
 
         // Plugin shutdown logic
 
-        String pversion = getConfig().getString("Version");
-        System.out.println(YELLOW_BOLD+"["+CYAN+"GuardianAngel"+YELLOW_BOLD+"] "+GREEN+"Version "+pversion+" has been Disabled"+RESET);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN.toString() + ChatColor.BOLD.toString() + "Disabling plugin....");
 
     }
 
